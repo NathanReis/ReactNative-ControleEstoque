62810b628f9437a07c2dad70ed821ebdab3a26f3
Nathan Reis <nathansilvareis@gmail.com>
2022-05-15 12:24:38 -0300

    Refatora e finaliza tela de listagem de categorias

--- mobile/src/screens/categories/index.tsx
+++ mobile/src/screens/categories/index.tsx
@@ -1,14 +1,15 @@
 import { useIsFocused, useNavigation } from '@react-navigation/native';
 import { useEffect, useState } from 'react';
-import { Alert, Text, View } from 'react-native';
+import { Alert, View } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 import SuccessButton from '../../components/button/success';
+import ListItem from '../../components/listItem';
 import Loading from '../../components/loading';
 import SafeZoneScreen from '../../components/safeZoneScreen';
 import ICategoryDto from '../../dtos/ICategoryDto';
+import IIdDto from '../../dtos/IIdDto';
 import ResponseHelper from '../../helpers/Response';
 import API from '../../services/api';
-import global from '../../styles/global';
 import styles from './styles';
 
 export default function Categories() {
@@ -36,6 +37,35 @@ export default function Categories() {
     navigation.navigate('Category' as never);
   }
 
+  function handleClickToEdit(id: string) {
+    navigation.navigate('Category' as never, { id } as never);
+  }
+
+  async function handleClickToRemove(id: string) {
+    Alert.alert(
+      'Confirme',
+      'Desajea realmente excluir esta categoria? Caso confirme, todos produtos serão excluídos!',
+      [
+        { text: 'Sim', onPress: async () => await remove(id) },
+        { text: 'Não' }
+      ]
+    );
+  }
+
+  async function remove(id: string) {
+    try {
+      await API.delete<IIdDto>(`/categories/${id}`);
+
+      Alert.alert('Excluído', 'Excluído com sucesso');
+
+      await fetchCategories();
+    } catch (error) {
+      let { title, message } = ResponseHelper.formatError(error);
+
+      Alert.alert(title, message);
+    }
+  }
+
   useEffect(() => {
     async function load() {
       setIsLoading(true);
@@ -61,9 +91,11 @@ export default function Categories() {
           data={categories}
           keyExtractor={item => item._id}
           renderItem={({ item }) => (
-            <View style={styles.card}>
-              <Text style={global.bold}>{item.description}</Text>
-            </View>
+            <ListItem
+              mainContent={item.description}
+              handleEdit={() => handleClickToEdit(item._id)}
+              handleRemove={() => handleClickToRemove(item._id)}
+            />
           )}
         />
       </View>
--- mobile/src/screens/categories/styles.ts
+++ mobile/src/screens/categories/styles.ts
@@ -1,18 +1,11 @@
 import { StyleSheet } from 'react-native';
-import colors from '../../styles/colors';
 import sizes from '../../styles/sizes';
 
 export default StyleSheet.create({
   listContainer: {
     height: '80%',
-    width: '100%',
+    width: sizes.listContainer.width,
 
-    marginTop: sizes.cardListContainer.margin.top
-  },
-  card: {
-    marginVertical: sizes.card.margin.vertical,
-    padding: sizes.card.padding,
-
-    backgroundColor: colors.card.background
+    marginTop: sizes.listContainer.margin.top
   }
 });
